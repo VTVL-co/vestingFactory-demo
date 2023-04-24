59c139a7c00c17d73b5e66419b2ab7102a840e7a
polartar <polaris.center@outlook.com>
2023-04-23 19:19:36 -0700

    implement factory with fund+vesting creation

--- src/connectors.js
+++ src/connectors.js
@@ -1,5 +1,5 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
+import { InjectedConnector } from '@web3-react/injected-connector';
 
 export const injectedConnector = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 43113],
+	supportedChainIds: [1, 3, 4, 5, 42, 43113, 84531]
 });
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,29 +2,40 @@ import { useState, useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { Contract } from '@ethersproject/contracts';
 import VestingFactoryABI from '../constants/VestingFactoryABI.json';
-import VestingContractABI from '../constants/Vesting.json';
 import ERC20ABI from '../constants/MockERC20ABI.json';
-import { BigNumber, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import { parseEther } from '@ethersproject/units';
 import { VESTING_FACTORY } from '../constants';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const iface = new ethers.utils.Interface(VestingFactoryABI);
+const iface = new ethers.utils.Interface(VestingFactoryABI.abi);
 
 function getParamFromEvent(transaction, eventName, paramIndex) {
 	const logs = transaction.logs.filter((l) =>
-		l.topics.includes(ethers.utils.id(eventName))
+		l.topics.includes(
+			'0xe5211516932e9042c46c90d9b551b41d1e991bc87e19bd664dc7b48f5323eef1'
+		)
 	);
+	console.log({ logs });
 	const event = iface.parseLog(logs[0]);
 	return event.args[paramIndex];
 }
+
+// function getPredictAddress(factoryAddress, salt, bytecode) {
+// 	const create2Inputs = ['0xff', factoryAddress, salt, keccak256(bytecode)];
+// 	const sanitizedInputs = `0x${create2Inputs.map((i) => i.slice(2)).join('')}`;
+// 	return getAddress(`0x${keccak256(sanitizedInputs).slice(-40)}`);
+// }
 
 const Homepage = () => {
 	const { library } = useWeb3React();
 	const [fundAddress, setFundAddress] = useState('');
 	const [vestingAddress, setVestingAddress] = useState('');
-	const [schedule, setSchedule] = useState({});
+	const [schedule, setSchedule] = useState({
+		linearAmount: '100',
+		cliffAmount: '1'
+	});
 	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
@@ -32,39 +43,6 @@ const Homepage = () => {
 			setVestingAddress(sessionStorage.getItem('vault'));
 		}
 	}, []);
-
-	const createVestingContract = async () => {
-		if (isLoading) return;
-		if (!fundAddress) {
-			alert('Please input Fund token address');
-			return;
-		}
-		if (library) {
-			const factoryInstance = new Contract(
-				VESTING_FACTORY,
-				VestingFactoryABI,
-				library.getSigner()
-			);
-			if (ethers.utils.isAddress(fundAddress)) {
-				setIsLoading(true);
-				const transaction = await factoryInstance.createVestingContract(
-					fundAddress
-				);
-
-				const address = getParamFromEvent(
-					await transaction.wait(),
-					'CreateVestingContract(address,address)',
-					0
-				);
-
-				setVestingAddress(address);
-				sessionStorage.setItem('vault', address);
-				setIsLoading(false);
-			} else {
-				alert('Please input valid fundAddress address');
-			}
-		}
-	};
 
 	const onInputChange = (e) => {
 		const { name, value } = e.target;
@@ -75,49 +53,38 @@ const Homepage = () => {
 		});
 	};
 
-	const fund = async () => {
-		if (!library) return;
-		const fundTokenInstance = new Contract(
-			fundAddress,
-			ERC20ABI,
-			library.getSigner()
-		);
-		try {
-			setIsLoading(true);
-			const trx = await fundTokenInstance.transfer(
-				vestingAddress,
-				parseEther(
-					(
-						Number(schedule.linearAmount) + Number(schedule.cliffAmount)
-					).toString()
-				)
-			);
-			await trx.wait();
-			alert('You deposited successfully');
-		} catch (err) {
-			alert(err.message);
-		}
-		setIsLoading(false);
-	};
-
 	const createSchedule = async () => {
 		if (!library) return;
+		if (!schedule.recipient) {
+			alert('Input recipient address');
+			return;
+		}
+		if (!schedule.linearAmount) {
+			alert('Input Linear amount');
+			return;
+		}
 		const fundTokenInstance = new Contract(
 			fundAddress,
 			ERC20ABI,
 			library.getSigner()
 		);
-		const balance = await fundTokenInstance.balanceOf(vestingAddress);
-		const requiredFund = BigNumber.from(schedule.linearAmount).add(
-			schedule.cliffAmount
+		// const nonce = await library.getTransactionCount(account);
+		// const salt = keccak256(
+		// 	solidityPack(['address', 'uint256'], [schedule.recipient, nonce])
+		// );
+
+		const tx = await fundTokenInstance.approve(
+			VESTING_FACTORY,
+			parseEther(
+				(
+					Number(schedule.linearAmount) + Number(schedule.cliffAmount)
+				).toString()
+			)
 		);
-		if (BigNumber.from(balance).sub(requiredFund).isNegative()) {
-			alert('You need to fund tokens');
-			return;
-		}
-		const vestingContractInstance = new Contract(
-			vestingAddress,
-			VestingContractABI,
+		await tx.wait();
+		const factoryInstance = new Contract(
+			VESTING_FACTORY,
+			VestingFactoryABI.abi,
 			library.getSigner()
 		);
 
@@ -128,16 +95,33 @@ const Homepage = () => {
 		);
 		try {
 			setIsLoading(true);
-			const trx = await vestingContractInstance.createClaim(
+
+			const trx = await factoryInstance.createVestingContract(
+				fundAddress,
+				parseEther(
+					(
+						Number(schedule.linearAmount) + Number(schedule.cliffAmount)
+					).toString()
+				),
 				schedule.recipient,
-				startTime,
-				endTime,
-				3600 * 24 * 30,
-				releaseInterval,
-				parseEther(schedule.linearAmount),
-				parseEther(schedule.cliffAmount)
+				[
+					startTime,
+					endTime,
+					3600 * 24 * 30,
+					releaseInterval,
+					parseEther(schedule.linearAmount),
+					parseEther(schedule.cliffAmount)
+				]
 			);
-			await trx.wait();
+			const address = getParamFromEvent(
+				await trx.wait(),
+				'CreateVestingContract(address,uint256,[uint40,uint40,uint40,uint40,uint256,uint112])',
+				0
+			);
+			console.log({ address });
+
+			setVestingAddress(address);
+			sessionStorage.setItem('vault', address);
 			alert('You created the schedule successfully');
 		} catch (err) {
 			alert(err.message);
@@ -149,100 +133,95 @@ const Homepage = () => {
 			<div style={{ marginLeft: '160px', marginBottom: '30px' }}>
 				{isLoading && <div className="loading-spinner"></div>}
 			</div>
-			{!vestingAddress && (
+
+			<div>
+				<p>Vesting Address: {vestingAddress}</p>
 				<div>
-					<input
-						placeholder="Fund token address"
-						className="wallet_input"
-						disabled={isLoading}
-						value={fundAddress}
-						onChange={(e) => setFundAddress(e.target.value)}
-					/>
-					<button onClick={(e) => createVestingContract()}>
-						Create vesting contract
-					</button>
-				</div>
-			)}
+					<div className="flex">
+						<span>Recipient: </span>
+						<input
+							type="text"
+							name="recipient"
+							value={schedule.recipient}
+							placeholder="Recipient Address"
+							onChange={(e) => onInputChange(e)}
+						></input>
+					</div>
+					<div className="flex">
+						<span>Fund token address: </span>
+						<input
+							placeholder="Fund token address"
+							className="wallet_input"
+							disabled={isLoading}
+							value={fundAddress}
+							onChange={(e) => setFundAddress(e.target.value)}
+						/>
+					</div>
+					<div className="flex">
+						<span>Start date: </span>
+						<DatePicker
+							selected={schedule.startDate}
+							onChange={(date) => setSchedule({ ...schedule, startDate: date })}
+						/>
+					</div>
+					<div className="flex">
+						<span>End date: </span>
+						<DatePicker
+							selected={schedule.endDate}
+							onChange={(date) => setSchedule({ ...schedule, endDate: date })}
+						/>
+					</div>
 
-			{vestingAddress && (
+					<div className="flex">
+						<span>cliffReleaseTimestamp: </span>
+						<span>1 month</span>
+					</div>
+
+					<div className="flex">
+						<span>Release frequency: </span>
+						<input
+							type="number"
+							name="releaseFrequency"
+							value={schedule.releaseFrequency}
+							onChange={(e) => onInputChange(e)}
+						></input>
+					</div>
+
+					<div className="flex">
+						<span>Linear Vesting amount: </span>
+						<input
+							type="number"
+							name="linearAmount"
+							value={schedule.linearAmount}
+							defaultValue={0}
+							onChange={(e) => onInputChange(e)}
+						></input>
+					</div>
+
+					<div className="flex">
+						<span>Cliff Amount: </span>
+						<input
+							type="number"
+							name="cliffAmount"
+							defaultValue={0}
+							value={schedule.cliffAmount}
+							onChange={(e) => onInputChange(e)}
+						></input>
+					</div>
+				</div>
 				<div>
-					<p>Vesting Address: {vestingAddress}</p>
-					<div>
-						<div className="flex">
-							<span>Recipient: </span>
-							<input
-								type="text"
-								name="recipient"
-								value={schedule.recipient}
-								placeholder="Recipient Address"
-								onChange={(e) => onInputChange(e)}
-							></input>
-						</div>
-						<div className="flex">
-							<span>Start date: </span>
-							<DatePicker
-								selected={schedule.startDate}
-								onChange={(date) =>
-									setSchedule({ ...schedule, startDate: date })
-								}
-							/>
-						</div>
-						<div className="flex">
-							<span>End date: </span>
-							<DatePicker
-								selected={schedule.endDate}
-								onChange={(date) => setSchedule({ ...schedule, endDate: date })}
-							/>
-						</div>
-
-						<div className="flex">
-							<span>cliffReleaseTimestamp: </span>
-							<span>1 month</span>
-						</div>
-
-						<div className="flex">
-							<span>Release frequency: </span>
-							<input
-								type="number"
-								name="releaseFrequency"
-								value={schedule.releaseFrequency}
-								onChange={(e) => onInputChange(e)}
-							></input>
-						</div>
-
-						<div className="flex">
-							<span>Linear Vesting amount: </span>
-							<input
-								type="number"
-								name="linearAmount"
-								value={schedule.linearAmount}
-								onChange={(e) => onInputChange(e)}
-							></input>
-						</div>
-
-						<div className="flex">
-							<span>Cliff Amount: </span>
-							<input
-								type="number"
-								name="cliffAmount"
-								value={schedule.cliffAmount}
-								onChange={(e) => onInputChange(e)}
-							></input>
-						</div>
-					</div>
-					<div>
-						Required fund:{' '}
-						{Number(schedule.linearAmount) + Number(schedule.cliffAmount)}
-					</div>
-					<button
-						style={{ margin: '10px' }}
-						onClick={(e) => fund()}
-					>
-						Fund
-					</button>
-					<button onClick={(e) => createSchedule()}>Create Schedule</button>
+					Required fund:{' '}
+					{Number(schedule.linearAmount) + Number(schedule.cliffAmount)}
 				</div>
-			)}
+				{/* <button
+					style={{ margin: '10px' }}
+					onClick={(e) => fund()}
+				>
+					Fund
+				</button> */}
+				<br></br>
+				<button onClick={(e) => createSchedule()}>Create Schedule</button>
+			</div>
 		</div>
 	);
 };
